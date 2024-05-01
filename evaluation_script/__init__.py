5d5131ece8f9a131df683ec01e6b1057564198b8
Jannik Zuern <jannik.zurn@wayve.ai>
2024-05-01 16:52:16 +0200

    replace sys executable

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -1,17 +1,10 @@
-"""
-# Q. How to install custom python pip packages?
-
-# A. Uncomment the below code to install the custom python packages.
-"""
 
 import os
 import subprocess
 import sys
 from pathlib import Path
 
-
-command = "python -m pip install -r {}".format(
-   os.path.join(str(Path(__file__).parent.parent.absolute()) + "/github/requirements.txt"))
+command = "{} -m pip install -r {}".format(sys.executable, os.path.join(str(Path(__file__).parent.parent.absolute()) + "/github/requirements.txt"))
 subprocess.run(command, shell=True, check=True)
 
 
