c6a19825f5eee3aa9d3aedb3a98f7e7fe0361ca6
Jannik Zuern <jannik.zurn@wayve.ai>
2024-05-01 16:40:24 +0200

    more pip install changes

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -9,38 +9,10 @@ import subprocess
 import sys
 from pathlib import Path
 
-def install(package):
-    # Install a pip python package
 
-    # Args:
-    #     package ([str]): Package name with version
-    print(f"Test-ABC Installing {package}")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+command = "python -m pip install -r {}".format(
+   os.path.join(str(Path(__file__).parent.parent.absolute()) + "/github/requirements.txt"))
+subprocess.run(command, shell=True, check=True)
 
-
-def install_local_package(folder_name):
-    # Install a local python package
-
-    # Args:
-    #     folder_name ([str]): name of the folder placed in evaluation_script/
-    
-    subprocess.check_output(
-    [
-        sys.executable,
-        "-m",
-        "pip",
-        "install",
-        os.path.join(str(Path(__file__).parent.absolute()) + folder_name),
-    ]
-)
-
-install("requests==2.24.0")
-install("torchmetrics[image]")
-install("pillow==9.4.0")
-install("torch==1.13.1")
-install("numpy==1.13.1")
-
-
-install_local_package("package_folder_name")
 
 from .main import evaluate
