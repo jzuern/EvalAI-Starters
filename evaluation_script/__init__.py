f8ac5061309b67a3f48aada79e5127154184ff99
Jannik Zuern <jannik.zurn@wayve.ai>
2024-05-01 17:37:18 +0200

    pip install in main.py

--- evaluation_script/__init__.py
+++ evaluation_script/__init__.py
@@ -1,11 +1,11 @@
 
-import os
-import subprocess
-import sys
-from pathlib import Path
+# import os
+# import subprocess
+# import sys
+# from pathlib import Path
 
-command = "{} -m pip install -r {}".format(sys.executable, os.path.join(str(Path(__file__).parent.parent.absolute()) + "/github/requirements.txt"))
-subprocess.run(command, shell=True, check=True)
+# command = "{} -m pip install -r {}".format(sys.executable, os.path.join(str(Path(__file__).parent.parent.absolute()) + "/github/requirements.txt"))
+# subprocess.run(command, shell=True, check=True)
 
 
-from .main import evaluate
+# from .main import evaluate
