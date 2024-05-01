69e838b2526226a0e62b385af363b0b9995af31e
Jannik Zuern <jannik.zurn@wayve.ai>
2024-05-01 12:17:18 +0200

    make eval script actually analyse images

--- evaluation_script/main.py
+++ evaluation_script/main.py
@@ -1,4 +1,8 @@
 import random
+import os
+import sys 
+
+from metrics import get_metrics
 
 
 def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwargs):
@@ -44,38 +48,40 @@ def evaluate(test_annotation_file, user_submission_file, phase_codename, **kwarg
         print("Evaluating for Dev Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
+                "train_split": get_metrics(test_annotation_file, user_submission_file)
             }
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]["train_split"]
         print("Completed evaluation for Dev Phase")
+        
+        
+        
     elif phase_codename == "test":
         print("Evaluating for Test Phase")
         output["result"] = [
             {
-                "train_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
+                "train_split": get_metrics(test_annotation_file, user_submission_file)
+
             },
             {
-                "test_split": {
-                    "Metric1": random.randint(0, 99),
-                    "Metric2": random.randint(0, 99),
-                    "Metric3": random.randint(0, 99),
-                    "Total": random.randint(0, 99),
-                }
+                "test_split": get_metrics(test_annotation_file, user_submission_file)
             },
         ]
         # To display the results in the result file
         output["submission_result"] = output["result"][0]
         print("Completed evaluation for Test Phase")
+        
     return output
+
+
+
+if __name__ == "__main__":
+    
+    test_annotation_file = "/Users/jannikzurn/Downloads/PSNR-example-base.png"
+    user_submission_file = "/Users/jannikzurn/Downloads/PSNR-example-comp-10.jpg"
+    
+    phase_codename = "dev"
+    evaluate(test_annotation_file, user_submission_file, phase_codename)
+    phase_codename = "test"
+    evaluate(test_annotation_file, user_submission_file, phase_codename)
